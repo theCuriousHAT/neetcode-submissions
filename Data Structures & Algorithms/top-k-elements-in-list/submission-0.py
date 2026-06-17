9ac46f0dd6a0f87f28323398b8e31bcaad163a5d
Ajay Singh Rawat <53109272+theCuriousHAT@users.noreply.github.com>
2026-06-17 20:16:47 +0530

    Add: top-k-elements-in-list - submission-0

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        
+        hashed={}
+        lista = []
+        for num in nums:
+            hashed[num] = 1 + hashed.get(num, 0)
+
+        for key, value in hashed.items():
+            lista.append([value, key])
+
+        lista.sort()
+
+        res = []
+
+        while len(res)<k:
+            res.append(lista.pop()[1])
+        return res
+
+        
